586c3b26fbc5f7949007dc1e96f7f5c75293f5fc
root <root@puppetmaster.tjnii.com>
2013-05-17 21:19:43 -0600

    Initial import

---
+++ manifests/client.pp
@@ -0,0 +1,42 @@
+class puppet::client {
+  # This is redundant, because you can't run this without puppet...
+  package { 'puppet':
+    ensure => installed,
+  }
+  
+  case $operatingsystem {
+      centos, redhat: {
+         service { 'puppet':
+            name      => 'puppet',
+            ensure    => running,
+            enable    => true,
+            subscribe => File['/etc/puppet/puppet.conf'],
+         }
+
+      }
+      debian: {
+         service { 'puppet':
+            name      => 'puppet',
+            ensure    => running,
+            enable    => true,
+            subscribe => [File['/etc/puppet/puppet.conf'], File['/etc/default/puppet']],
+         }
+
+         file { '/etc/default/puppet':
+            path    => '/etc/default/puppet',
+            ensure  => file,
+            require => Package['puppet'],
+            source  => "puppet:///modules/puppet/puppet.defaults.debian",
+         }
+      }
+  }
+
+  file { '/etc/puppet/puppet.conf':
+    path    => '/etc/puppet/puppet.conf',
+    ensure  => file,
+    require => Package['puppet'],
+    source  => "puppet:///modules/puppet/puppet.client.conf",
+  }
+
+}
+                          
