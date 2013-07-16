929c29581fd02fe7b2b31e7639af037200b96689
Tom Noonan II <root@tjnii.com>
2013-07-16 09:44:02 -0500

    Made client puppetmaster hostname a variable

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,11 +1,13 @@
 class puppet::client {
   class {'puppet::common': }
+
+  $puppetmaster = "puppetmaster.tjnii.com"
   
   file { '/etc/puppet/puppet.conf':
     path    => '/etc/puppet/puppet.conf',
     ensure  => file,
     require => Package['puppet'],
-    source  => "puppet:///modules/puppet/puppet.client.conf",
+    content => template("puppet/puppet.client.conf.erb"),
   }
 
 }
